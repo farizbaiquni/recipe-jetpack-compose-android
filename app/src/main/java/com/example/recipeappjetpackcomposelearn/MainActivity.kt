de0e99b5529aba88a570dc2f7a62ec4e057a3a27
Fariz Baiquni <farizbaiquni@gmail.com>
2022-02-18 23:19:55 +0700

    add hilt and retrofit dependency

--- app/src/main/java/com/example/recipeappjetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/com/example/recipeappjetpackcomposelearn/MainActivity.kt
@@ -9,15 +9,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.recipeappjetpackcomposelearn.ui.theme.RecipeAppJetpackComposeLearnTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var example: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RecipeAppJetpackComposeLearnTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting(example)
                 }
             }
         }
